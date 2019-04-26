99c0afb9600ad427d55445b214120c289da15241
madrahul1992 <madrahul1992@gmail.com>
2019-04-26 20:37:04 +0530

    Remove files

--- src/main/java/com/journaldev/bootifulmongodb/interceptor/Chain.java
+++
@@ -1,6 +0,0 @@
-package com.journaldev.bootifulmongodb.interceptor;
-
-public interface Chain {
-    public abstract void setNext(Chain nextInChain);
-    public abstract void process(Number request);
-}
